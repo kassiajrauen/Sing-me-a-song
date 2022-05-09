aa17b2cc319b60fa86137511e940980ae7eaa617
KassiaJRauen <kassia.grj@gmail.com>
2022-05-09 02:14:00 -0300

    add integration POST /recommendations

--- back-end/tests/all.test.ts
+++ back-end/tests/all.test.ts
@@ -1,14 +1,35 @@
 import app from "../src/app.js";
 import supertest from "supertest";
 import {prisma} from "../src/database.js";
+import recommendationsBodyFactory from "./factories/recommendationBodyFactory.js";
 
-describe("test", async () => {
+describe("POST /recommendations", () => {
+  beforeEach(async () => {
+    await prisma.$executeRaw`TRUNCATE TABLE recommendations;`;
+  });
 
-    beforeEach(async () => {
-        await prisma.$executeRaw`TRUNCATE TABLE receitas;`;
-      });
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
 
-      afterAll(async () => {
-        await prisma.$disconnect();
-   });
+  it("should return 201 given a valid body", async () => {
+    const music = recommendationsBodyFactory();
+
+    const response = await supertest(app)
+      .post("/recommendations").send(music);
+    const createdMusic = await prisma.recommendation.findUnique({ 
+      where: { name: music.name}
+    })
+
+    expect(createdMusic).not.toBeNull();
+    expect(response.status).toEqual(201);
+  })
+
+  it("should return 422 given an invalid body", async () => {
+    const music = recommendationsBodyFactory();
+
+    const response = await supertest(app).post("/recommendations").send(music);
+
+    expect(response.status).toEqual(201);
+  })
 });
---
+++ back-end/tests/factories/recommendationBodyFactory.ts
@@ -0,0 +1,9 @@
+import {faker} from '@faker-js/faker'
+
+export default function recommendationBodyFactory(){
+    const music = {
+        name: faker.lorem.sentence(2),
+        youtubeLink: "https://www.youtube.com/watch?v=pnKNMa12ggQ"
+    }
+    return music;
+}
