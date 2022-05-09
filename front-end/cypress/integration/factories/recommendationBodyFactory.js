7b4f47b45c02f980f9f47d7a48bd780c2d54ea2c
KassiaJRauen <kassia.grj@gmail.com>
2022-05-08 21:41:36 -0300

    add e2e insert new recommendation

---
+++ front-end/cypress/integration/factories/recommendationBodyFactory.js
@@ -0,0 +1,7 @@
+export default function recommendationBodyFactory() {
+  const music = {
+    name: "Falamansa - Xote dos Milagres",
+    youtubeLink: "https://www.youtube.com/watch?v=chwyjJbcs1Y",
+  };
+  return music;
+}
---
+++ front-end/cypress/integration/home.spec.js
@@ -0,0 +1,26 @@
+import recommendationBodyFactory from "./factories/recommendationBodyFactory.js";
+
+describe("Home page", () => {
+  beforeEach(() => {
+    cy.request("POST", "http://localhost:5000/recommendations/reset", {});
+  });
+
+  it("should insert a new recommendation successfully", () => {
+    const music = recommendationBodyFactory();
+
+    cy.visit("http://localhost:3000");
+
+    cy.get("#name").type(music.name);
+    cy.get("#link").type(music.youtubeLink);
+
+    cy.intercept("POST", "http://localhost:5000/recommendations").as(
+      "insertNewRecommendation"
+    );
+
+    cy.get("Button").click();
+
+    cy.wait("@insertNewRecommendation");
+
+    cy.end();
+  });
+});
